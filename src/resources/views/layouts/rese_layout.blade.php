39acc2432eef0f2743b61f835fe01beaa79c610d
suzuki chiemi <thankschimail@gmail.com>
2024-10-24 22:31:45 +0900

    login.css作成

--- src/resources/views/layouts/rese_layout.blade.php
+++ src/resources/views/layouts/rese_layout.blade.php
@@ -9,6 +9,7 @@
     <!-- Robotoフォントを読み込む -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
     @yield('css')
--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -6,23 +6,24 @@
 
 @section('content')
 
-<form class="login__form">
-    <h2 class="login__form-title">Login</h2>
-    <div class="login__form-group">
-        <label for="email" class="login__label">
-            <i class="login__icon login__icon--email"></i>
-            Email
-        </label>
-        <input type="email" id="email" name="email" class="login__input">
-    </div>
-    <div class="login__form-group">
-        <label for="password" class="login__label">
-            <i class="login__icon login__icon--password"></i>
-            Password
-        </label>
-        <input type="password" id="password" name="password" class="login__input">
-    </div>
-    <button type="submit" class="login__button">ログイン</button>
-</form>
-
+<div class="login__container">
+    <form class="login__form">
+        <h2 class="login__form-title">Login</h2>
+        <div class="login__form-group">
+            <label for="email" class="login__label">
+                <img src="{{ asset('image/mail-image-20241022.png') }}" alt="メールアイコン" class="login__icon">
+                Email
+            </label>
+            <input type="email" id="email" name="email" class="login__input">
+        </div>
+        <div class="login__form-group">
+            <label for="password" class="login__label">
+                <img src="{{ asset('image/key-image-20241022.png') }}" alt="パスワードアイコン" class="login__icon">
+                Password
+            </label>
+            <input type="password" id="password" name="password" class="login__input">
+        </div>
+        <button type="submit" class="login__button">ログイン</button>
+    </form>
+</div>
 @endsection
