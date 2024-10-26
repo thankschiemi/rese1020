7ec53e1bbd0c55fd166686aa4423a2d1402a2b1d
suzuki chiemi <thankschimail@gmail.com>
2024-10-26 22:50:48 +0900

    login.cssとregister.css作成し完成

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -10,15 +10,13 @@
     <form class="login__form">
         <h2 class="login__form-title">Login</h2>
 
-        <!-- Email フィールド -->
         <div class="login__form-group">
             <label for="email" class="login__label">
                 <img src="{{ asset('image/mail-image-20241022.png') }}" alt="メールアイコン" class="login__icon">
             </label>
-            <input type="text" name="email" placeholder="Email" class="login__input">
+            <input type="email" name="email" placeholder="Email" class="login__input">
         </div>
 
-        <!-- Password フィールド -->
         <div class="login__form-group">
             <label for="password" class="login__label">
                 <img src="{{ asset('image/key-image-20241022.png') }}" alt="パスワードアイコン" class="login__icon">
@@ -26,7 +24,6 @@
             <input type="password" name="password" placeholder="Password" class="login__input">
         </div>
 
-        <!-- ログインボタン -->
         <button type="submit" class="login__button">ログイン</button>
     </form>
 </div>
--- src/resources/views/register.blade.php
+++ src/resources/views/register.blade.php
@@ -6,30 +6,33 @@
 
 @section('content')
 
-<form class="register__form">
-    <h2 class="register__form-title">Register</h2>
-    <div class="register__form-group">
-        <label for="username" class="register__label">
-            <i class="register__icon register__icon--user"></i>
-            ユーザー名
-        </label>
-        <input type="text" id="username" name="username" class="register__input">
-    </div>
-    <div class="register__form-group">
-        <label for="email" class="register__label">
-            <i class="register__icon register__icon--email"></i>
-            メールアドレス
-        </label>
-        <input type="email" id="email" name="email" class="register__input">
-    </div>
-    <div class="register__form-group">
-        <label for="password" class="register__label">
-            <i class="register__icon register__icon--password"></i>
-            パスワード
-        </label>
-        <input type="password" id="password" name="password" class="register__input">
-    </div>
-    <button type="submit" class="register__button">登録</button>
-</form>
+<div class="register__container">
+    <form class="register__form">
+        <h2 class="register__form-title">Register</h2>
 
-@endsection
+
+        <div class="register__form-group">
+            <label for="username" class="register__label">
+                <img src="{{ asset('image/person-image-20241022.png') }}" alt="メンバーアイコン" class="register__icon">
+            </label>
+            <input type="name" name="name" placeholder="Username" class="register__input">
+        </div>
+
+        <div class="register__form-group">
+            <label for="email" class="register__label">
+                <img src="{{ asset('image/mail-image-20241022.png') }}" alt="メールアイコン" class="register__icon">
+            </label>
+            <input type="email" name="email" placeholder="Email" class="register__input">
+        </div>
+
+        <div class="register__form-group">
+            <label for="password" class="register__label">
+                <img src="{{ asset('image/key-image-20241022.png') }}" alt="パスワードアイコン" class="register__icon">
+            </label>
+            <input type="password" name="password" placeholder="Password" class="register__input">
+        </div>
+
+        <button type="submit" class="register__button">登録</button>
+    </form>
+
+    @endsection
