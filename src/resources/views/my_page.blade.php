229a4fc3f3adbe3e489ec783d78d143e67a8e7b6
suzuki chiemi <thankschimail@gmail.com>
2024-11-05 14:57:26 +0900

    my_page.css完成

--- src/resources/views/my_page.blade.php
+++ src/resources/views/my_page.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.rese_layout')
 
 @section('css')
+@section('css')
 <link rel="stylesheet" href="{{ asset('css/common_restaurant.css') }}">
 <link rel="stylesheet" href="{{ asset('css/my_page.css') }}">
+@endsection
+
 @endsection
 
 @section('content')
@@ -10,7 +13,7 @@
     <div class="mypage__content">
         <!-- 左カラム：予約情報 -->
         <section class="mypage__reservations">
-            <h2 class="mypage__section-title">予約状況</h2>
+            <h2 class="mypage__status-title">予約状況</h2>
             @forelse ($reservations as $reservation)
             <div class="reservation-card">
                 <div class="reservation__card-header">
@@ -18,15 +21,27 @@
                         <img src="{{ asset('image/clock-image-20241022.png') }}" alt="時計アイコン">
                     </div>
                     <h3 class="reservation__card-title">予約{{ $loop->iteration }}</h3>
-                    <button class="reservation__close-button">
-                        <img src="{{ asset('image/Butt_white-image-20241022.png') }}" alt="閉じるボタン">
-                    </button>
                 </div>
+                <button class="reservation__close-button">
+                    <div class="close-icon"></div>
+                </button>
                 <ul class="reservation-card__details">
-                    <li class="reservation-card__detail">Shop: {{ $reservation->restaurant->name }}</li>
-                    <li class="reservation-card__detail">Date: {{ $reservation->reservation_date }}</li>
-                    <li class="reservation-card__detail">Time: {{ $reservation->reservation_time }}</li>
-                    <li class="reservation-card__detail">Number: {{ $reservation->number_of_people }}人</li>
+                    <li class="reservation-card__detail">
+                        <span class="detail-label">Shop</span>
+                        <span class="detail-value">{{ $reservation->restaurant->name }}</span>
+                    </li>
+                    <li class="reservation-card__detail">
+                        <span class="detail-label">Date</span>
+                        <span class="detail-value">{{ $reservation->reservation_date }}</span>
+                    </li>
+                    <li class="reservation-card__detail">
+                        <span class="detail-label">Time</span>
+                        <span class="detail-value">{{ $reservation->reservation_time }}</span>
+                    </li>
+                    <li class="reservation-card__detail">
+                        <span class="detail-label">Number</span>
+                        <span class="detail-value">{{ $reservation->number_of_people }}人</span>
+                    </li>
                 </ul>
             </div>
             @empty
