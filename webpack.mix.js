fa089a137ea843e98f65269b4bdb63517beb426e
suzuki chiemi <thankschimail@gmail.com>
2024-11-13 21:28:31 +0900

    Updated CSS files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,9 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
-   .vue()
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js('src/resources/js/app.js', 'src/public/js')
+   .vue() 
+   .sass('src/resources/sass/app.scss', 'src/public/css')
+   .styles([
+       'src/public/css/common.css',
+       'src/public/css/login.css'
+   ], 'src/public/css/all.css');
