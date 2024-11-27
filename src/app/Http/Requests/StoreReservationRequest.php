dff8d355bb24181179b4bd8c7bcf65787593bfc0
suzuki chiemi <thankschimail@gmail.com>
2024-11-27 20:56:08 +0900

    予約日時のバリデーションを修正して現在の日時以降のみ予約可能に設定

--- src/app/Http/Requests/StoreReservationRequest.php
+++ src/app/Http/Requests/StoreReservationRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests;
 
-
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreReservationRequest extends FormRequest
@@ -18,6 +17,7 @@ class StoreReservationRequest extends FormRequest
             'date' => 'required|date|after_or_equal:today',
             'time' => 'required|date_format:H:i',
             'number' => 'required|integer|min:1|max:10',
+            'datetime' => 'nullable|date|after:now',
         ];
     }
 
@@ -29,6 +29,28 @@ class StoreReservationRequest extends FormRequest
             'date.after_or_equal' => '過去の日付を選択することはできません。',
             'time.required' => '時間を選択してください。',
             'number.required' => '人数を選択してください。',
+            'number.integer' => '人数は数字で入力してください。',
+            'datetime.after' => '現在の日時以降を選択してください。',
         ];
+    }
+
+    protected function prepareForValidation()
+    {
+        // 入力された日付と時間を結合して datetime を作成
+        $this->merge([
+            'datetime' => $this->input('date') . ' ' . $this->input('time') . ':00',
+        ]);
+    }
+
+    protected function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $datetime = $this->input('datetime');
+
+            // 現在の日時より前の場合はエラーを追加
+            if (strtotime($datetime) < strtotime('now')) {
+                $validator->errors()->add('time', '現在の日時以降を選択してください。');
+            }
+        });
     }
 }
