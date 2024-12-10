cb775e01c6b47d1c835e07325e11f0d2e1ab14aa
suzuki chiemi <thankschimail@gmail.com>
2024-12-10 10:19:12 +0900

    タブレット・スマートフォン用のレスポンシブデザインを作成中　会員登録画面完成

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('reminder:send')->dailyAt('08:00');
+        $schedule->command('reminder:send')->dailyAt('15:00');
     }
 
     /**
--- src/resources/views/restaurant_all.blade.php
+++ src/resources/views/restaurant_all.blade.php
@@ -9,32 +9,35 @@
 <div class="header_filters">
     <form method="GET" action="{{ route('restaurants.index') }}" class="filter">
         <div class="filter-wrapper">
-            <select name="region_id" class="filter_area" onchange="this.form.submit()">
-                <option value="">All area</option>
-                @foreach ($regions as $region)
-                <option value="{{ $region->id }}" {{ request('region_id') == $region->id ? 'selected' : '' }}>
-                    {{ $region->name }}
-                </option>
-                @endforeach
-            </select>
-            <div class="dropdown-icon">▼</div>
+            <div class="filter-select">
+                <select name="region_id" class="filter_area" onchange="this.form.submit()">
+                    <option value="">All area</option>
+                    @foreach ($regions as $region)
+                    <option value="{{ $region->id }}" {{ request('region_id') == $region->id ? 'selected' : '' }}>
+                        {{ $region->name }}
+                    </option>
+                    @endforeach
+                </select>
+                <div class="dropdown-icon">▼</div>
+            </div>
         </div>
-
         <div class="filter-wrapper">
-            <select name="genre_id" class="filter_genre" onchange="this.form.submit()">
-                <option value="">All genre</option>
-                @foreach ($genres as $genre)
-                <option value="{{ $genre->id }}" {{ request('genre_id') == $genre->id ? 'selected' : '' }}>
-                    {{ $genre->name }}
-                </option>
-                @endforeach
-            </select>
-            <div class="dropdown-icon">▼</div>
+            <div class="filter-select">
+                <select name="genre_id" class="filter_genre" onchange="this.form.submit()">
+                    <option value="">All genre</option>
+                    @foreach ($genres as $genre)
+                    <option value="{{ $genre->id }}" {{ request('genre_id') == $genre->id ? 'selected' : '' }}>
+                        {{ $genre->name }}
+                    </option>
+                    @endforeach
+                </select>
+                <div class="dropdown-icon">▼</div>
+            </div>
         </div>
-
         <div class="search-container">
+            <label for="keyword" class="sr-only">Search</label>
             <img src="{{ asset('image/magnifying-glass-icon.png') }}" alt="Search Icon" class="search-icon">
-            <input type="search" name="keyword" class="filter_search" placeholder="Search ..." value="{{ request('keyword') }}">
+            <input type="search" id="keyword" name="keyword" class="filter_search" placeholder="Search ..." value="{{ request('keyword') }}">
         </div>
     </form>
 </div>
@@ -44,25 +47,21 @@
 <div class="page_content">
     @forelse ($restaurants as $restaurant)
     <article class="restaurant">
-        <div class="restaurant__image">
-            <img src="{{ $restaurant->image_url }}" alt="店舗画像" class="login_icon">
-        </div>
+        <img src="{{ $restaurant->image_url }}" alt="店舗画像" class="restaurant__image">
         <div class="restaurant__details">
             <h2 class="restaurant__name">{{ $restaurant->name }}</h2>
             <p class="restaurant__tags">#{{ $restaurant->region->name }} #{{ $restaurant->genre->name }}</p>
             <div class="restaurant_buttons">
-                <a href="{{ route('restaurants.detail', $restaurant->id) }}" class="restaurant_button">詳しくみる</a>
-
+                <a href="{{ route('restaurants.detail', $restaurant->id) }}" class="restaurant_button" aria-label="詳しくみるボタン">詳しくみる</a>
                 @if (Auth::check())
                 <form action="{{ route('favorites.store', ['restaurant_id' => $restaurant->id]) }}" method="POST">
                     @csrf
-                    <button class="restaurant_favorite-button {{ $restaurant->is_favorite ? 'active' : '' }}">❤</button>
+                    <button class="restaurant_favorite-button {{ $restaurant->is_favorite ? 'active' : '' }}" aria-label="お気に入り追加">❤</button>
                 </form>
                 @else
-                <a href="{{ route('account-settings') }}" class="restaurant_favorite-button">❤️</a>
+                <a href="{{ route('account-settings') }}" class="restaurant_favorite-button" aria-label="ログインページへのリンク">❤️</a>
                 @endif
             </div>
-
         </div>
     </article>
     @empty
